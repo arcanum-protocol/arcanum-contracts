8ab6c6b3dded258c452c0e8bdbfd5b7d2af64bfc
Artemiy Artamonov <array.clean@gmail.com>
2023-08-28 23:33:23 +0300

    OPS: update ci (#17)

--- tests/utils/numbers.ts
+++
@@ -1,8 +0,0 @@
-import { BigNumber, BigNumberish, FixedNumber } from "ethers";
-
-export function toDecimal(
-  number: BigNumberish,
-  decimals: Number = 18
-): BigNumber {
-  return BigNumber.from(number).mul(BigNumber.from(10).pow(BigNumber.from(decimals)));
-}
