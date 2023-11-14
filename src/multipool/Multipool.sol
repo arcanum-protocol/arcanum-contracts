4c1aab09047fb660b611d325af11e887ecd2621e
Artemiy Artamonov <array.clean@gmail.com>
2023-11-14 12:28:51 +0300

    MULTIPOOL: add state (#32)

--- src/multipool/Multipool.sol
+++ src/multipool/Multipool.sol
@@ -73,6 +73,14 @@ contract Multipool is
     bool public isPaused;
     bool public audited;
 
+    // added fields
+    struct FeedInfo {
+        uint fixedValue;
+        uint twapRange;
+        address v3Address;
+    }
+    mapping(address => FeedInfo) public prices;
+
     modifier notPaused() {
         require(!isPaused, "MULTIPOOL: IP");
         _;
@@ -143,6 +151,28 @@ contract Multipool is
         returns (uint amount)
     {
         amount = (share * context.usdCap * DENOMINATOR) / mpTotalSupply / asset.price;
+    }
+
+    function getSqrtTwapX96(address uniswapV3Pool, uint32 twapInterval) public view returns (uint160 sqrtPriceX96) {
+        if (twapInterval == 0) {
+            // return the current price if twapInterval == 0
+            (sqrtPriceX96, , , , , , ) = IUniswapV3Pool(uniswapV3Pool).slot0();
+        } else {
+            uint32[] memory secondsAgos = new uint32[](2);
+            secondsAgos[0] = twapInterval; // from (before)
+            secondsAgos[1] = 0; // to (now)
+
+            (int56[] memory tickCumulatives, ) = IUniswapV3Pool(uniswapV3Pool).observe(secondsAgos);
+
+            // tick(imprecise as it's an integer) to price
+            sqrtPriceX96 = TickMath.getSqrtRatioAtTick(
+                int24((tickCumulatives[1] - tickCumulatives[0]) / twapInterval)
+            );
+        }
+    }
+
+    function getPriceX96FromSqrtPriceX96(uint160 sqrtPriceX96) public pure returns(uint256 priceX96) {
+        return FullMath.mulDiv(sqrtPriceX96, sqrtPriceX96, FixedPoint96.Q96);
     }
 
     function massiveMint(address[] calldata assetAddresses, address to)
