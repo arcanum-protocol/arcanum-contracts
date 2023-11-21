db2f03aff25ef30e288c0df23d5031f7d197bc6b
Artemiy Artamonov <array.clean@gmail.com>
2023-11-21 23:52:05 +0300

    MULTIPOOL: snapshots (#32)

---
+++ src/lib/MpContext.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {FixedPoint96} from "./FixedPoint96.sol";
+import "../multipool/Multipool.sol";
+
+struct MpAsset {
+    uint quantity;
+    uint128 share;
+    uint128 collectedCashbacks;
+}
+
+struct MpContext {
+    uint sharePrice;
+    uint oldTotalSupply;
+    int totalSupplyDelta;
+    uint totalTargetShares;
+    uint deviationParam;
+    uint deviationLimit;
+    uint depegBaseFee;
+    uint baseFee;
+    int unusedEthBalance;
+    uint totalCollectedCashbacks;
+    uint collectedFees;
+    uint cummulativeInAmount;
+    uint cummulativeOutAmount;
+}
+
+using {
+    ContextMath.calculateFees, ContextMath.calculateFeesShareToken, ContextMath.applyCollected
+} for MpContext global;
+
+library ContextMath {
+    function subAbs(uint a, uint b) internal pure returns (uint c) {
+        c = a > b ? a - b : b - a;
+    }
+
+    function pos(int a) internal pure returns (uint b) {
+        b = a > 0 ? uint(a) : uint(-a);
+    }
+
+    function addDelta(uint a, int b) internal pure returns (uint c) {
+        c = b > 0 ? a + uint(b) : a - uint(-b);
+    }
+
+    function calculateFeesShareToken(MpContext memory ctx, int quantityDelta) internal view {
+        uint fee = ((pos(quantityDelta) * ctx.sharePrice * ctx.baseFee) >> 32 ) >> FixedPoint96.RESOLUTION;
+        ctx.unusedEthBalance -= int(fee);
+        ctx.collectedFees += fee;
+    }
+
+    function calculateFees(MpContext memory ctx, MpAsset memory asset, int quantityDelta, uint price) internal view {
+        uint newQuantity = addDelta(asset.quantity, quantityDelta);
+        uint newTotalSupply = addDelta(ctx.oldTotalSupply, ctx.totalSupplyDelta);
+        uint targetShare = (asset.share << 32) / ctx.totalTargetShares;
+
+        uint dOld = subAbs(
+            ctx.oldTotalSupply == 0 ? 0 : (asset.quantity * price << 32) / ctx.oldTotalSupply / ctx.sharePrice,
+            targetShare
+        );
+        uint dNew = subAbs(
+            newTotalSupply == 0 ? 0 : (newQuantity * price << 32) / newTotalSupply / ctx.sharePrice, targetShare
+        );
+        uint quotedDelta = (pos(quantityDelta) * price) >> FixedPoint96.RESOLUTION;
+
+        uint bf = (ctx.baseFee * quotedDelta) >> 32;
+        ctx.collectedFees += bf;
+        ctx.unusedEthBalance -= int(bf);
+        if (dNew > dOld) {
+            if (!(ctx.deviationLimit >= dNew)) revert DeviationExceedsLimit();
+            uint deviationFee = (ctx.deviationParam * dNew * quotedDelta / (ctx.deviationLimit - dNew)) >> 32;
+            uint basePart = (deviationFee * ctx.depegBaseFee) >> 32;
+            ctx.unusedEthBalance -= int(deviationFee);
+            ctx.collectedFees += basePart;
+            ctx.totalCollectedCashbacks += (deviationFee - basePart);
+
+            asset.collectedCashbacks += uint128(deviationFee - basePart);
+        } else {
+            uint cashback = (dOld - dNew) * asset.collectedCashbacks / dOld;
+
+            ctx.unusedEthBalance += int(cashback);
+            ctx.totalCollectedCashbacks -= cashback;
+
+            asset.collectedCashbacks -= uint128(cashback);
+        }
+        asset.quantity = newQuantity;
+    }
+
+    function applyCollected(MpContext memory ctx, address payable refundTo) internal {
+        int balance = ctx.unusedEthBalance;
+        if (!(balance >= 0)) revert FeeExceeded();
+        if (balance > 0) {
+            refundTo.transfer(uint(balance));
+        }
+    }
+}
--- src/multipool/Multipool.sol
+++ src/multipool/Multipool.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import {ERC20, IERC20} from "openzeppelin/token/ERC20/ERC20.sol";
-import {MpAsset, MpContext} from "./MpMath.sol";
-import {FeedInfo, FeedType} from "./PriceMath.sol";
+import {MpAsset, MpContext} from "../lib/MpContext.sol";
+import {FeedInfo, FeedType} from "../lib/Price.sol";
 
 import {ERC20Upgradeable} from "oz-proxy/token/ERC20/ERC20Upgradeable.sol";
 import {ERC20PermitUpgradeable} from "oz-proxy/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
@@ -13,7 +13,18 @@ import {OwnableUpgradeable} from "oz-proxy/access/OwnableUpgradeable.sol";
 import {Initializable} from "oz-proxy/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "oz-proxy/proxy/utils/UUPSUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "oz-proxy/utils/ReentrancyGuardUpgradeable.sol";
-import { FixedPoint96 } from "uniswapv3/libraries/FixedPoint96.sol";
+import {FixedPoint96} from "../lib/FixedPoint96.sol";
+
+import { ECDSA } from "openzeppelin/utils/cryptography/ECDSA.sol";
+import { MessageHashUtils } from "openzeppelin/utils/cryptography/MessageHashUtils.sol";
+
+error InvalidForcePushAuthoritySignature();
+error ForcePushedPriceExpired();
+error DeviationExceedsLimit();
+error FeeExceeded();
+error ZeroAmountSupplied();
+error InsuficcientBalance();
+error SleepageExceeded();
 
 /// @custom:security-contact badconfig@arcanum.to
 contract Multipool is
@@ -24,13 +35,18 @@ contract Multipool is
     UUPSUpgradeable,
     ReentrancyGuardUpgradeable
 {
-    function initialize(string memory mpName, string memory mpSymbol, address owner, uint sharePrice) public initializer {
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
+
+    function initialize(string memory mpName, string memory mpSymbol, address owner, uint sharePrice)
+        public
+        initializer
+    {
         __ERC20_init(mpName, mpSymbol);
         __ERC20Permit_init(mpName);
         __ReentrancyGuard_init();
         __Ownable_init(owner);
         initialSharePrice = sharePrice;
-        targetShareAuthority = owner;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
@@ -42,22 +58,22 @@ contract Multipool is
     mapping(address => MpAsset) internal assets;
     mapping(address => FeedInfo) internal prices;
 
-    uint internal totalTargetShares;
+    uint public totalTargetShares;
 
-    uint64 internal halfDeviationFee;
+    uint64 internal deviationParam;
     uint64 internal deviationLimit;
     uint64 internal depegBaseFee;
     uint64 internal baseFee;
 
-    uint internal constant DENOMINATOR = 1e18;
-
-    address public targetShareAuthority;
     bool public isPaused;
 
-    uint internal totalCollectedCashbacks;
-    uint internal collectedFees;
+    uint public totalCollectedCashbacks;
+    uint public collectedFees;
 
     uint internal initialSharePrice;
+    uint internal sharePriceTTL;
+
+    mapping(address => bool) internal isPriceSetter;
 
     modifier notPaused() {
         _;
@@ -65,8 +81,12 @@ contract Multipool is
 
     // ---------------- Methods ------------------
 
-    function getFees() public view returns (uint64 _halfDeviationFee, uint64 _deviationLimit, uint64 _depegBaseFee, uint64 _baseFee) {
-        _halfDeviationFee = halfDeviationFee;
+    function getFees()
+        public
+        view
+        returns (uint64 _deviationParam, uint64 _deviationLimit, uint64 _depegBaseFee, uint64 _baseFee)
+    {
+        _deviationParam = deviationParam;
         _deviationLimit = deviationLimit;
         _depegBaseFee = depegBaseFee;
         _baseFee = baseFee;
@@ -76,54 +96,61 @@ contract Multipool is
         asset = assets[assetAddress];
     }
 
-    function getContext() internal view returns (MpContext memory context) {
-        uint ts = totalSupply();
-        (uint64 _halfDeviationFee, uint64 _deviationLimit, uint64 _depegBaseFee, uint64 _baseFee) = getFees();
-        context = MpContext({
-           sharePrice: ts == 0 ? initialSharePrice: prices[address(this)].getPrice(),
-           oldTotalSupply: ts,
-           totalSupplyDelta: 0,
-           totalTargetShares: totalTargetShares,
-           deviationParam: _halfDeviationFee * DENOMINATOR / _deviationLimit,
-           deviationLimit: _deviationLimit,
-           depegBaseFee: _depegBaseFee,
-           baseFee: _baseFee,
-           feeToPay: 0,
-           cashbackDelta: 0,
-           feeDelta: 0,
-           totalCollectedCashbacks: totalCollectedCashbacks,
-           collectedFees: collectedFees,
-           cummulativeInAmount: 0,
-           cummulativeOutAmount: 0
-       });
+    function getContext(FPSharePriceArg calldata fpSharePrice) internal view returns (MpContext memory ctx) {
+        uint totSup = totalSupply();
+        uint price;
+        if (fpSharePrice.thisAddress == address(this)) {
+            bytes memory data = abi.encodePacked(fpSharePrice.thisAddress, fpSharePrice.timestamp, fpSharePrice.value);
+            if (!isPriceSetter[keccak256(data).toEthSignedMessageHash().recover(fpSharePrice.signature)]) 
+                revert InvalidForcePushAuthoritySignature();
+            if (block.timestamp - fpSharePrice.timestamp >= sharePriceTTL)
+                revert ForcePushedPriceExpired();
+            price = fpSharePrice.value;
+        } else {
+            price = totSup == 0 ? initialSharePrice : prices[address(this)].getPrice();
+        }
+        (uint64 _deviationParam, uint64 _deviationLimit, uint64 _depegBaseFee, uint64 _baseFee) = getFees();
+        ctx.sharePrice = price;
+        ctx.oldTotalSupply = totSup;
+        ctx.totalTargetShares = totalTargetShares;
+        ctx.deviationParam = _deviationParam;
+        ctx.deviationLimit = _deviationLimit;
+        ctx.depegBaseFee = _depegBaseFee;
+        ctx.baseFee = _baseFee;
+        ctx.totalCollectedCashbacks = totalCollectedCashbacks;
+        ctx.collectedFees = collectedFees;
+        ctx.unusedEthBalance = int(address(this).balance - ctx.totalCollectedCashbacks - ctx.collectedFees);
     }
 
     function getTransferredAmount(MpAsset memory asset, address assetAddress) internal view returns (uint amount) {
         amount = IERC20(assetAddress).balanceOf(address(this)) - asset.quantity;
     }
 
-    function getPricesAndSumQuotes(
-        MpContext memory ctx, 
-        AssetArg[] memory selectedAssets
-    ) internal view returns (uint[] memory pr) {
+    function getPricesAndSumQuotes(MpContext memory ctx, AssetArg[] memory selectedAssets)
+        internal
+        view
+        returns (uint[] memory pr)
+    {
         uint arrayLen = selectedAssets.length;
         pr = new uint[](arrayLen);
         for (uint i; i < arrayLen;) {
-            uint price; 
-            if (selectedAssets[i].addr == address(this)) { 
+            uint price;
+            if (selectedAssets[i].addr == address(this)) {
                 price = ctx.sharePrice;
                 ctx.totalSupplyDelta -= selectedAssets[i].amount;
             } else {
                 price = prices[selectedAssets[i].addr].getPrice();
             }
             pr[i] = price;
-            require(selectedAssets[i].amount != 0, "ASSET AMOUNT EQ 0");
+            if (selectedAssets[i].amount == 0) revert ZeroAmountSupplied();
             if (selectedAssets[i].amount > 0) {
                 ctx.cummulativeInAmount += price * uint(selectedAssets[i].amount) >> FixedPoint96.RESOLUTION;
             } else {
                 ctx.cummulativeOutAmount += price * uint(-selectedAssets[i].amount) >> FixedPoint96.RESOLUTION;
             }
-            unchecked { ++i; }
+            unchecked {
+                ++i;
+            }
         }
     }
 
@@ -132,72 +159,78 @@ contract Multipool is
         int amount;
     }
 
+    struct FPSharePriceArg {
+        address thisAddress;
+        uint128 timestamp;
+        uint128 value;
+        bytes signature;
+    }
+
     function swap(
-        AssetArg[] calldata selectedAssets, 
+        FPSharePriceArg calldata fpSharePrice,
+        AssetArg[] calldata selectedAssets,
         bool isSleepageReverse,
         address to,
         bool refundDust,
         address refundTo
-    )
-        public
-        payable
-        notPaused
-        nonReentrant
-    {
-        MpContext memory ctx = getContext();
+    ) public payable notPaused nonReentrant {
+        MpContext memory ctx = getContext(fpSharePrice);
         uint[] memory currentPrices = getPricesAndSumQuotes(ctx, selectedAssets);
 
         for (uint i; i < selectedAssets.length;) {
-            MpAsset memory asset; 
+            address tokenAddress = selectedAssets[i].addr;
+            int suppliedAmount = selectedAssets[i].amount;
+            MpAsset memory asset;
             if (selectedAssets[i].addr != address(this)) {
                 asset = assets[selectedAssets[i].addr];
             }
-            uint price = currentPrices[i]; 
-            int deltaAmount = selectedAssets[i].amount;
+            uint price = currentPrices[i];
             if (!isSleepageReverse) {
-                if (selectedAssets[i].amount > 0) {
-                    uint transferred = getTransferredAmount(asset, selectedAssets[i].addr);
-                    require(transferred >= uint(selectedAssets[i].amount), "INSUFFICIENT TRANSFERRED");
-                    if (refundDust) {
-                        IERC20(selectedAssets[i].addr).transfer(to, transferred - uint(selectedAssets[i].amount));
+                if (suppliedAmount > 0) {
+                    uint transferred = getTransferredAmount(asset, tokenAddress);
+                    if (!(transferred >= uint(suppliedAmount))) revert InsuficcientBalance();
+                    if (refundDust && (transferred - uint(suppliedAmount)) > 0) {
+                        IERC20(tokenAddress).transfer(refundTo, transferred - uint(suppliedAmount));
                     }
                 } else {
-                    uint amount = ctx.cummulativeInAmount * uint(-selectedAssets[i].amount) / ctx.cummulativeOutAmount;
+                    uint amount = ctx.cummulativeInAmount * uint(-suppliedAmount) / ctx.cummulativeOutAmount;
 
-                    require(int(amount) >= selectedAssets[i].amount, "SLIPPAGE ON OUT");
-                    if (selectedAssets[i].addr != address(this)) {
-                        IERC20(selectedAssets[i].addr).transfer(refundTo, amount);
+                    if (!(int(amount) >= suppliedAmount)) revert SleepageExceeded();
+                    if (tokenAddress != address(this)) {
+                        IERC20(tokenAddress).transfer(to, amount);
                     }
 
-                    deltaAmount = -int(amount);
+                    suppliedAmount = -int(amount);
                 }
             } else {
-                if (selectedAssets[i].amount > 0) {
-                    uint amount = ctx.cummulativeOutAmount * uint(selectedAssets[i].amount) / ctx.cummulativeInAmount;
+                if (suppliedAmount > 0) {
+                    uint amount = ctx.cummulativeOutAmount * uint(suppliedAmount) / ctx.cummulativeInAmount;
 
-                    uint transferred = getTransferredAmount(asset, selectedAssets[i].addr);
-                    require(amount <= uint(selectedAssets[i].amount), "SLIPPAGE ON IN");
-                    require(transferred >= amount, "INSUFFICIENT TRANSFERRED");
-                    if (refundDust) {
-                        IERC20(selectedAssets[i].addr).transfer(refundTo, transferred - amount);
+                    uint transferred = getTransferredAmount(asset, tokenAddress);
+                    if (!(amount <= uint(suppliedAmount))) revert SleepageExceeded();
+                    if (!(transferred >= amount)) revert InsuficcientBalance();
+                    if (refundDust && (transferred - amount) > 0) {
+                        IERC20(tokenAddress).transfer(refundTo, transferred - amount);
                     }
 
-                    deltaAmount = int(amount);
+                    suppliedAmount = int(amount);
                 } else {
-                    if (selectedAssets[i].addr != address(this)) {
-                        IERC20(selectedAssets[i].addr).transfer(to, uint(-selectedAssets[i].amount));
+                    if (tokenAddress != address(this)) {
+                        IERC20(tokenAddress).transfer(to, uint(-suppliedAmount));
                     }
                 }
             }
-            if (selectedAssets[i].addr != address(this)) {
-                ctx.calculateFees(asset, deltaAmount, price);
-                assets[selectedAssets[i].addr] = asset;
+            if (tokenAddress != address(this)) {
+                ctx.calculateFees(asset, suppliedAmount, price);
+                assets[tokenAddress] = asset;
             } else {
-                ctx.calculateFeesShareToken(deltaAmount);
+                ctx.calculateFeesShareToken(suppliedAmount);
             }
-            unchecked { ++i; }
+            unchecked {
+                ++i;
+            }
         }
-        ctx.applyCollected();
+        ctx.applyCollected(payable(refundTo));
         if (ctx.totalSupplyDelta > 0) {
             _mint(to, uint(ctx.totalSupplyDelta));
         } else if (ctx.totalSupplyDelta < 0) {
@@ -208,27 +241,25 @@ contract Multipool is
         collectedFees = ctx.collectedFees;
     }
 
-    // ---------------- Authorities ------------------
-
     function increaseCashback(address assetAddress) public notPaused nonReentrant returns (uint amount) {
         MpAsset storage asset = assets[assetAddress];
         amount = getTransferredAmount(asset, assetAddress);
-        asset.collectedCashbacks += amount;
+        asset.collectedCashbacks += uint128(amount);
     }
 
-    function updatePrice(address assetAddress, FeedType kind, bytes calldata feedData) public notPaused {
-        prices[assetAddress] = FeedInfo ({
-            kind: kind,
-            data: feedData
-        });
+    // ---------------- Owner ------------------
+
+    function updatePrice(address assetAddress, FeedType kind, bytes calldata feedData) public onlyOwner notPaused {
+        prices[assetAddress] = FeedInfo({kind: kind, data: feedData});
     }
 
     function updateTargetShares(address[] calldata assetAddresses, uint[] calldata shares) public onlyOwner notPaused {
-        require(targetShareAuthority == msg.sender, "MULTIPOOL: TA");
-        for (uint a = 0; a < assetAddresses.length; a++) {
+        uint len = assetAddresses.length;
+        for (uint a; a < len;) {
             MpAsset storage asset = assets[assetAddresses[a]];
             totalTargetShares = totalTargetShares - asset.share + shares[a];
-            asset.share = shares[a];
+            asset.share = uint128(shares[a]); 
+            unchecked { ++a; }
         }
     }
 
@@ -238,16 +269,28 @@ contract Multipool is
         payable(to).transfer(fees);
     }
 
-     // ---------------- Owner ------------------
 
     function togglePause() external onlyOwner {
         isPaused = !isPaused;
     }
 
-    function setCurveParams(uint64 newDeviationLimit, uint64 newHalfDeviationFee, uint64 newDepegBaseFee, uint64 newBaseFee) external onlyOwner {
+    function setCurveParams(
+        uint64 newDeviationLimit,
+        uint64 newHalfDeviationFee,
+        uint64 newDepegBaseFee,
+        uint64 newBaseFee
+    ) external onlyOwner {
         deviationLimit = newDeviationLimit;
-        halfDeviationFee = newHalfDeviationFee;
+        deviationParam = (newHalfDeviationFee << 32) / newDeviationLimit;
         depegBaseFee = newDepegBaseFee;
         baseFee = newBaseFee;
+    }
+
+    function setSharePriceTTL(uint newSharePriceTTL) external onlyOwner {
+        sharePriceTTL = newSharePriceTTL;
+    }
+
+    function toggleForcePushAuthority(address authority) external onlyOwner {
+         isPriceSetter[authority] = !isPriceSetter[authority];
     }
 }
