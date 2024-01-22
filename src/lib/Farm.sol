e62b4dab4cb912abb05b43b0a80d8af5e00257f6
Art <array.clean@gmail.com>
2024-01-22 23:17:12 +0300

    FARMING: add pure tests (#48)

--- src/lib/Farm.sol
+++ src/lib/Farm.sol
@@ -18,37 +18,51 @@ struct PoolInfo {
     uint lastUpdateBlock;
 }
 
+using {
+    FarmingMath.deposit,
+    FarmingMath.withdraw,
+    FarmingMath.updateDistribution
+}  for PoolInfo global;
+
 library FarmingMath {
 
-    function updatePool(PoolInfo memory pool, uint currentBlockNumber) public pure {
+    function updatePool(PoolInfo memory pool, uint currentBlockNumber) internal pure {
 
         uint rewards = pool.rpb * (currentBlockNumber - pool.lastUpdateBlock);
         if (rewards > pool.availableRewards) {
             rewards = pool.availableRewards;
         }
-        pool.arps +=  rewards * 1e18 / pool.lockAssetTotalNumber;
+
+        pool.lastUpdateBlock = currentBlockNumber;
+        if (pool.lockAssetTotalNumber > 0) {
+            pool.arps +=  rewards * 1e18 / pool.lockAssetTotalNumber;
+            pool.availableRewards -= rewards;
+        } else {
+            pool.arps = 0;
+        }
     }
 
-    function updateRewards(PoolInfo memory pool, UserInfo memory user, uint currentBlockNumber) public pure {
+    function updateRewards(PoolInfo memory pool, UserInfo memory user, uint currentBlockNumber) internal pure {
         updatePool(pool, currentBlockNumber);
-        uint amount = user.amount * pool.arps - user.rd;
-        user.rd += amount;
+        uint amount = user.amount * pool.arps / 1e18 - user.rd;
         user.accRewards += amount;
     }
 
-    function deposit(PoolInfo memory pool, UserInfo memory user, uint currentBlockNumber, uint depositAmount) public pure {
+    function deposit(PoolInfo memory pool, UserInfo memory user, uint currentBlockNumber, uint depositAmount) internal pure {
         updateRewards(pool, user, currentBlockNumber); 
         user.amount += depositAmount;
+        user.rd = user.amount * pool.arps / 1e18;
         pool.lockAssetTotalNumber += depositAmount;
     }
 
-    function withdraw(PoolInfo memory pool, UserInfo memory user, uint currentBlockNumber, uint withdrawAmount) public pure {
+    function withdraw(PoolInfo memory pool, UserInfo memory user, uint currentBlockNumber, uint withdrawAmount) internal pure {
         updateRewards(pool, user, currentBlockNumber); 
         user.amount -= withdrawAmount;
+        user.rd = user.amount * pool.arps / 1e18;
         pool.lockAssetTotalNumber -= withdrawAmount;
     }
 
-    function updateDistribution(PoolInfo memory pool, uint currentBlockNumber, int rewardsDelta, uint newRpb) public pure {
+    function updateDistribution(PoolInfo memory pool, uint currentBlockNumber, int rewardsDelta, uint newRpb) internal pure {
         updatePool(pool, currentBlockNumber);
         if (rewardsDelta >= 0) {
             pool.availableRewards += uint(rewardsDelta);
